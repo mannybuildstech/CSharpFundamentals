63e8d1341338ee15456eff628803844496c3e29b
Manuel Arredondo <manuelucf@gmail.com>
2015-03-25 07:47:17 -0400

    - Created outline of projects to implement, separated by concepts to explore - Continued to go through https://msdn.microsoft.com/en-us/library/67ef8sbd.aspx finding interesting previously unknown facts

--- CSharpIntro/Program.cs
+++ CSharpIntro/Program.cs
@@ -6,45 +6,7 @@ using System.Threading.Tasks;
 
 namespace CSharpIntro
 {
-    class Program
-    {
-        //args array contains command-line arguments excluding executable
-        //executable name is included in c++ args
-        static int Main(string[] args)
-        {
-            Console.WriteLine("--  int  --");
-            Console.WriteLine("minimum:"+int.MinValue);
-            Console.WriteLine("maximum:" + int.MaxValue);
-            Console.WriteLine("length:" + sizeof(int));
-            Console.WriteLine();
-
-            Console.WriteLine("--  float  --");
-            Console.WriteLine("minimum:" + float.MinValue);
-            Console.WriteLine("maximum:" + float.MaxValue);
-            Console.WriteLine("length:" + sizeof(float));
-            Console.WriteLine();
-
-            //Because a string "modification" is actually a new string creation, you must use 
-            //caution when you create references to strings. If you create a reference to a string, and then "modify" the original string, 
-            //the reference will continue to point to the original object instead of the new object that was created when the string was modified. The following code illustrates this behavior:
-            string name = "coder";
-            string nameCopy = name;
-            
-            //this call creates a new string in memory with both strings combined
-            // previous value is still in memory until the garbage collector gets rid of it
-            name+= " maximus";
-
-            Console.WriteLine("variable nameCopy still holds old value:"+nameCopy); 
-            
-            //asks console user for character input, preventing program from quitting
-            Console.ReadLine();
-
-            /*programs' main function can be void or return an int. It could be read by the caller of the program.
-              example: a command-line interface returns error codes that are used to determine the next call in a batch file */
-            return 0;
-        }
-
-        /* value types are primitive containers:
+    /* value types are primitive containers:
          * 
          *      Integral - can only express whole numbers
          * 
@@ -67,13 +29,57 @@ namespace CSharpIntro
          *  byte
          * */
 
-        /*  user defined types include: 
-         *      - enums 
-         *      - structs (all numeric value types are structs)
-         *      - classes, interfaces (see hierarchy project)
-         * 
-         */
-        
+    /*  user defined types include: 
+     *      - enums 
+     *      - structs (all numeric value types are structs)
+     *      - classes, interfaces (see hierarchy project)
+     * 
+     */
+
+    class Program
+    {
+        //args array contains command-line arguments excluding executable
+        //executable name is included in c++ args
+        static int Main(string[] args)
+        {
+            Console.WriteLine("--  int  --");
+            Console.WriteLine("minimum:"+int.MinValue);
+            Console.WriteLine("maximum:" + int.MaxValue);
+            Console.WriteLine("length:" + sizeof(int));
+            Console.WriteLine();
+
+            Console.WriteLine("--  float  --");
+            Console.WriteLine("minimum:" + float.MinValue);
+            Console.WriteLine("maximum:" + float.MaxValue);
+            Console.WriteLine("length:" + sizeof(float));
+            Console.WriteLine();
+
+            //Because a string "modification" is actually a new string creation, you must use 
+            //caution when you create references to strings. If you create a reference to a string, and then "modify" the original string, 
+            //the reference will continue to point to the original object instead of the new object that was created when the string was modified. The following code illustrates this behavior:
+            string name = "coder";
+            string nameCopy = name;     
+            //this call creates a new string in memory with both strings combined
+            // previous value is still in memory until the garbage collector gets rid of it
+            name+= " maximus";
+            Console.WriteLine("variable nameCopy still holds old value:"+nameCopy);
+
+            Programmer manny = new Programmer();
+            manny.Name = "manny";
+            DotNetLanguage languageOfPreference = manny.languageOfPreference();
+
+            if (languageOfPreference != DotNetLanguage.CSHARP)
+                Console.WriteLine("You know nothing!");
+
+
+            //asks console user for character input, preventing program from quitting
+            Console.ReadLine();
+
+            /*programs' main function can be void or return an int. It could be read by the caller of the program.
+              example: a command-line interface returns error codes that are used to determine the next call in a batch file */
+            return 0;
+        }
+
         enum DotNetLanguage
         {
             CSHARP,
@@ -87,14 +93,19 @@ namespace CSharpIntro
         /// </summary>
         struct Programmer : LazyHuman
         {
-            TimeSpan yearsOfExperience;
-            TimeSpan timeSinceGraduation;
-            Guid uniqueIdentifier;
-            string name;
-
-            bool writeCode(string code)
+            public TimeSpan yearsOfExperience;
+            public TimeSpan timeSinceGraduation;
+            public Guid uniqueIdentifier;
+            public string Name;
+            
+            public DotNetLanguage languageOfPreference()
             {
-                Console.WriteLine(name + " wrote the following code:" + Environment.NewLine + "\t");
+                return DotNetLanguage.CSHARP;
+            }
+
+            public bool writeCode(string code)
+            {
+                Console.WriteLine(Name + " wrote the following code:" + Environment.NewLine + "\t");
                 return true;
             }
 
@@ -102,12 +113,49 @@ namespace CSharpIntro
             {
                 return new TimeSpan(12,0,0);
             }
-        }
 
+        }
+        
+        //see inheritance project for more information
         interface LazyHuman
         {
             TimeSpan sleep();
         }
+        
+        /*
+         *  Interesting Statements -->
+         * 
+         *  checked\unchecked -- by default, overflow exceptions are not checked unless you explicitly use this statement:
+         */
+         void checkedOverflow()
+         {
+            int x = Int32.MaxValue;
+            try
+            {
+                Console.WriteLine(checked(x * 2)); 
+            }
+            catch (System.OverflowException e)
+            {
+                Console.WriteLine("Overflow exception thrown:" + e.Message);
+            }
+         }
 
+        //The foreach statement is used to iterate through the collection to get the information that you want, but can not be used to add or remove items from the 
+        //source collection to avoid unpredictable side effects. If you need to add or remove items from the source collection, use a for loop.
+        bool isProgrammerThere(string Name,Programmer[]coders)
+        {
+            bool result = false;
+            //must implement System.Collections.IEnumerable in order to be compatible with foreach
+            // implementation would need an additional class that implements IEnumerable, this would not work 
+            foreach(Programmer coder in coders)
+            {
+                if(coder.Name.CompareTo(Name)==0)
+                {
+                    result = true;
+                    break;
+                }
+            }
+            return result;
+        }
     }
 }
---
+++ Inheritance/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    class Program
+    {
+        /*
+         *                                                                  Classes vs Structs
+         *           
+         * In short: classes (by reference\pointers) structs (by value)
+         * 
+         * A class is a reference type. When an object of the class is created, the variable to which the object is assigned holds only a reference to that memory. 
+         * When the object reference is assigned to a new variable, the new variable refers to the original object. Changes made through one variable are reflected 
+         * in the other variable because they both refer to the same data.
+           A struct is a value type. When a struct is created, the variable to which the struct is assigned holds the struct's actual data. When the struct is assigned 
+         * to a new variable, it is copied. The new variable and the original variable therefore contain two separate copies of the same data. Changes made to one copy 
+         * do not affect the other copy.
+           In general, classes are used to model more complex behavior, or data that is intended to be modified after a class object is created. Structs are best suited 
+         * for small data structures that contain primarily data that is not intended to be modified after the struct is created.
+        */
+
+        /* 3 pillars of Object Oriented Programming:
+         * 
+         *  encapsulation - classes can control how much access is given to outside code
+         *  inheritance   - 
+         *  polymorphism  -
+         *  
+         */ 
+
+        static void Main(string[] args)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
